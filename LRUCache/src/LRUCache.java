53639b1d3c77dd3b71404876bef7f24cca37fa1d
Mikhail_Mac <mikhail-dssv@rambler.ru>
2023-06-01 19:12:22 +0300

    Реализация LRUCache через LinkedHashMap

--- LRUCache/src/LRUCache.java
+++ LRUCache/src/LRUCache.java
@@ -1,22 +1,50 @@
-public class LRUCache {
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+class LRUCache {
+    private int capacity;
+    private Map<String, String> cache;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        cache = new LinkedHashMap<>();
+    }
+
+    public String get(String key) {
+        if (cache.containsKey(key)) {
+            String value = cache.get(key);
+            cache.remove(key);
+            cache.put(key, value);
+            return value;
+        }
+        return "";
+    }
+
+    public void put(String key, String value) {
+        if (cache.size() >= capacity && !cache.containsKey(key))
+            cache.remove(cache.entrySet().iterator().next().getKey());
+
+        if (cache.containsKey(key))
+            cache.remove(key);
+
+        cache.put(key, value);
+    }
+
+    public void rem(String key) {
+        if (cache.containsKey(key)) {
+            cache.remove(key);
+        }
+    }
 
     public static void main(String[] args) {
-        Cache cache = new Cache(100);
-        cache.set("Jesse", "Pinkman");
-        cache.set("Walter", "White");
-        cache.set("Jessie", "James");
+        LRUCache lruCache = new LRUCache(10);
 
-       // lru.getAll();
+        lruCache.put("Jesse", "Pinkman");
+        lruCache.put("Walter", "White");
+        lruCache.put("Jesse", "James");
 
-        System.out.println("Get Jessie: " + cache.get("Jessie") + "\n");
-
-       // lru.getAll();
-
-        cache.rem("Walter");
-
-        System.out.println("Get Walter: " + cache.get("Walter") + "\n");
-
-       // lru.getAll();
+        System.out.println("Get Jesse: " + lruCache.get("Jesse"));
+        lruCache.rem("Walter");
+        System.out.println("Get Walter: " + lruCache.get("Walter"));
     }
 }
