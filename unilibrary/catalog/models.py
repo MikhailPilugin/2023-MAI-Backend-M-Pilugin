4cb998af5e6fba44f5654ebd21ad1b54b3a00f94
Mikhail VirtualBox <tonopod8@yandex.ru>
2023-06-15 14:39:04 +0000

    Docker, docker-compose

--- unilibrary/catalog/models.py
+++ unilibrary/catalog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+import uuid
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -43,3 +44,33 @@ class Author(models.Model):
             "date_of_birth": str(self.date_of_birth),
             "date_of_death": str(self.date_of_death)
         }
+    
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
+
+    def __str__(self):
+        return self.name
+    
+
+class BookInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book availability')
+
+    class Meta:
+        ordering = ["due_back"]
+
+
+    def __str__(self):
+        return '%s (%s)' % (self.id,self.book.title)
